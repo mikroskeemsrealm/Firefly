4f9ee4c4c26b54a65a3ae6353745f2b925106092
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2020-05-08 22:29:09 +0300

    Drop kyori text library from chat subproject dependencies

--- gradle/subprojects/chat-build.gradle.kts
+++ gradle/subprojects/chat-build.gradle.kts
@@ -1,10 +1,4 @@
-val kyoriTextVersion = "3.0.2"
-
 dependencies {
-    api("net.kyori:text-api:${kyoriTextVersion}")
-    api("net.kyori:text-serializer-gson:${kyoriTextVersion}")
-    api("net.kyori:text-serializer-legacy:${kyoriTextVersion}")
-
     // Updated dependencies
     api("com.google.code.gson:gson:2.8.5")
 }
